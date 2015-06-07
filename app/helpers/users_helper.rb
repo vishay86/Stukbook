8cfff2233c87f7b888ac46ae0275f7fc9c816b1f
Vishay Bhatia <vishay@addictivegifts.com>
2015-06-07 15:52:21 +0400

    Added friendship status detection

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,17 @@
 module UsersHelper
+
+	def action_buttons(user)
+		case current_user.friendship_status(user) 
+			when "friends"
+			 "Remove Friendship"
+			when "pending"
+			 "Cancel Request"
+			when "requested"
+			 "Accept or Deny"
+			when "not_friends"
+		   "Add as Friend"
+		end
+	end	
 end
+
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,19 +11,33 @@ class User < ActiveRecord::Base
   has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id', dependent: :destroy
 
 
-def request_friendship(user_2)
-	self.friendships.create(friend: user_2)
-end
+	def request_friendship(user_2)
+		self.friendships.create(friend: user_2)
+	end
 
-def pending_friend_requests_from
-	self.inverse_friendships.where(state: 'pending')
-end
+	def pending_friend_requests_from
+		self.inverse_friendships.where(state: 'pending')
+	end
 
-def pending_friend_requests_to
-	self.friendships.where(state: 'pending')
-end
+	def pending_friend_requests_to
+		self.friendships.where(state: 'pending')
+	end
 
-def active_friends
-	self.friendships.where(state: 'active').map(&:friend) + self.inverse_friendships.where(state: 'active').map(&:user)
-end
+	def friendship_status(user_2)
+    friendship = Friendship.where(user_id: [self.id,user_2.id], friend_id: [self.id,user_2.id])
+    unless friendship.any?
+      return "not_friends"
+    else
+      if friendship.first.state == "active"
+        return "friends"
+      else
+        if friendship.first.user == self
+          return "pending"
+        else
+          return "requested"
+        end
+      end
+    end
+  end
+	
 end
